3e88d2a74787478c8f42807d7e12104603f29dec
Daniel Rojo Pérez <danielrojo@gmail.com>
2024-09-30 11:35:35 +0200

    first commit

---
+++ src/main/java/com/example/demo/GreetingController.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String getMethodName(
+        @RequestParam(name = "msg", required = false, defaultValue = "Hello World") String message, 
+        @RequestParam(name = "num", required = false, defaultValue = "2.0") float number,
+        Model view) {
+        view.addAttribute("data", message);
+        view.addAttribute("num", number);
+        return "greeting";
+    }
+    
+    
+    
+}
