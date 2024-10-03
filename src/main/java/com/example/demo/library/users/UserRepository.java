b3088012295c5da1f6ab291bdbae40080be48353
Daniel Rojo Pérez <danielrojo@gmail.com>
2024-10-03 11:12:14 +0200

    openapi docs

--- src/main/java/com/example/demo/library/users/UserRepository.java
+++ src/main/java/com/example/demo/library/users/UserRepository.java
@@ -3,9 +3,10 @@ package com.example.demo.library.users;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<UserEntity, Long> {
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByEmail(String email);
     List<UserEntity> findByName(String name);
     Optional<UserEntity> findByNameAndEmail(String name, String email);
